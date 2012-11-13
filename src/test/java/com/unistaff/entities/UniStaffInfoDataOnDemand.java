1676fce79e78d38dd30a1a3dc39b8465634e2872
Dto <dto80@yahoo.com>
2012-11-13 22:40:46 +0700

    first class creation

---
+++ src/test/java/com/unistaff/entities/UniStaffInfoDataOnDemand.java
@@ -0,0 +1,7 @@
+package com.unistaff.entities;
+
+import org.springframework.roo.addon.dod.RooDataOnDemand;
+
+@RooDataOnDemand(entity = UniStaffInfo.class)
+public class UniStaffInfoDataOnDemand {
+}
---
+++ src/test/java/com/unistaff/entities/UniStaffInfoIntegrationTest.java
@@ -0,0 +1,12 @@
+package com.unistaff.entities;
+
+import org.junit.Test;
+import org.springframework.roo.addon.test.RooIntegrationTest;
+
+@RooIntegrationTest(entity = UniStaffInfo.class)
+public class UniStaffInfoIntegrationTest {
+
+    @Test
+    public void testMarkerMethod() {
+    }
+}
