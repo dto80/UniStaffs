d104f849ba8a754e80bc91e6573c5238f6891c0c
Dto-Intellij <dto80@yahoo.com>
2012-12-08 12:29:05 +0700

    commit the some file creation/ move to SpringSourceTools

--- src/main/java/com/unistaff/entities/Address.java
+++ src/main/java/com/unistaff/entities/Address.java
@@ -1,12 +1,8 @@
 package com.unistaff.entities;
 
-import com.unistaff.entities.address.District;
-import com.unistaff.entities.address.Province;
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
 import org.springframework.roo.addon.tostring.RooToString;
-
-import javax.persistence.ManyToOne;
 
 @RooJavaBean
 @RooToString
@@ -21,9 +17,4 @@ public class Address {
 
     private String tumbon;
 
-    @ManyToOne
-    private District district;
-
-    @ManyToOne
-    private Province province;
 }
--- src/main/java/com/unistaff/entities/BloodTypeEnum.java
+++
@@ -1,7 +0,0 @@
-package com.unistaff.entities;
-
-
-public enum BloodTypeEnum {
-
-    A, B, O, AB;
-}
